14f45d310f05567a1a7aa35b8ad252172c731e6b
Steve Dougherty <steve@asksteved.com>
2021-12-28 06:21:16 -0500

    Document SendMessage() argument constants

--- ScreenSleep/Screen.cs
+++ ScreenSleep/Screen.cs
@@ -14,9 +14,18 @@ namespace ScreenSleep
         [DllImport("user32.dll")]
         private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
 
+        // See https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessage
+        private const int Broadcast = 0xFFFF;
+
+        // See https://docs.microsoft.com/en-us/windows/win32/menurc/wm-syscommand
+        // WM_SYSCOMMAND
+        private const int WmSyscommand = 0x112;
+        // SC_MONITORPOWER
+        private const int ScMonitorPower = 0xF170;
+
         public static void SetScreenState(ScreenState state)
         {
-            SendMessage(0xFFFF, 0x112, 0xF170, (int)state);
+            SendMessage(Broadcast, WmSyscommand, ScMonitorPower, (int)state);
         }
 
     }
